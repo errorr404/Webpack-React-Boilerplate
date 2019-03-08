b427909ec6292820f438d95ccf95f1686f781748
errorr404 <dkbishwash@gmail.com>
2019-03-08 11:35:57 +0530

    added a component folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HelloWorld from './components/HelloWorld';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+            <HelloWorld />
+            </div>
+        )
+    }
+}
+
+export default App
